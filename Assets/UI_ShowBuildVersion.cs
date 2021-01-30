4b166331a4561ad1785fc6c42d3f0e7cf8039241
Chanisco Tromp <Chanisco@live.nl>
2021-01-30 12:36:01 +0100

    Created layout for the Initial UI

---
+++ Assets/UI_ShowBuildVersion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class UI_ShowBuildVersion : MonoBehaviour
+{
+    public TMP_Text buildText;
+
+    private void Start()
+    {
+        if (buildText == null)
+        {
+            buildText.GetComponent<TMP_Text>();
+        }
+        buildText.text = "Version : " + Application.version;
+    }
+}
